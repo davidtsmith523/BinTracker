ac6239e7a1f295b2d32a26f83d4ca0edd681aebb
davidtsmith523 <davidtsmith523@gmail.com>
2023-08-05 19:53:03 -0500

    Added the folders that I will need for the app

---
+++ constants/images.tsx
@@ -0,0 +1,6 @@
+//import cardboardBoxes from '../assets/images/cardboard-boxes.png';
+const cardboardBoxes = require('../assets/images/cardboard-boxes.png') as string;
+
+export default {
+  cardboardBoxes,
+};
